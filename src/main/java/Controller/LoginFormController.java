d9de8018b6101675d4170f8414fd188fe31a1ba5
Buddheema Ryan <buddimaraya16@gmail.com>
2025-11-12 12:59:07 +0530

    Initial Commit

---
+++ src/main/java/Controller/LoginFormController.java
@@ -0,0 +1,21 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnLogin(ActionEvent event) {
+
+    }
+
+}
