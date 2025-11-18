b1d8bafc917470f594c2329c9c6289f7be4de71c
Buddheema Ryan <buddimaraya16@gmail.com>
2025-11-19 00:23:24 +0530

    OrderDetailsDTO Complete

--- src/main/java/model/OrderDetails.java
+++ src/main/java/model/OrderDetails.java
@@ -1,4 +1,16 @@
 package model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class OrderDetails {
+    private String orderId;
+    private String itemCode;
+    private int orderQty;
+    private int discount;
+
 }
