2b9fdb5654a525826a4cec3534f7c3ceb62e7071
Buddheema Ryan <buddimaraya16@gmail.com>
2025-11-19 00:15:29 +0530

    CustomerDTO complete

--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -1,4 +1,20 @@
 package model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Customer {
+    private String custId;
+    private String cusTitle;
+    private String custName;
+    private String dob;
+    private double salary;
+    private String custAddress;
+    private String city;
+    private String province;
+    private String postalCode;
 }
