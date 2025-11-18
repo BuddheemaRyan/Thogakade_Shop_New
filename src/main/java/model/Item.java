7956b45fa405142f906b0791c273a8b2ebb8aad7
Buddheema Ryan <buddimaraya16@gmail.com>
2025-11-19 00:19:58 +0530

    itemDTO Complete

--- src/main/java/model/Item.java
+++ src/main/java/model/Item.java
@@ -1,4 +1,16 @@
 package model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Item {
+    private String itemCode;
+    private String description;
+    private String packSize;
+    private double unitPrice;
+    private int qtyOnHand;
 }
