dfc7a6fe423dbd30d46a4a60ea970e48f9a874b0
Buddheema Ryan <buddimaraya16@gmail.com>
2025-11-19 16:58:24 +0530

    Add CustomerService Interface with methods

--- src/main/java/Controller/Customer/CustomerFormController.java
+++ src/main/java/Controller/Customer/CustomerFormController.java
@@ -1,4 +1,100 @@
 package Controller.Customer;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class CustomerFormController {
+
+    @FXML
+    private TableColumn<?, ?> col_address;
+
+    @FXML
+    private TableColumn<?, ?> col_city;
+
+    @FXML
+    private TableColumn<?, ?> col_cust_id;
+
+    @FXML
+    private TableColumn<?, ?> col_dob;
+
+    @FXML
+    private TableColumn<?, ?> col_name;
+
+    @FXML
+    private TableColumn<?, ?> col_postal_code;
+
+    @FXML
+    private TableColumn<?, ?> col_province;
+
+    @FXML
+    private TableColumn<?, ?> col_salary;
+
+    @FXML
+    private TableColumn<?, ?> col_title;
+
+    @FXML
+    private TableView<?> tblCustomer;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtCity;
+
+    @FXML
+    private TextField txtCustId;
+
+    @FXML
+    private DatePicker txtDOB;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPostalCode;
+
+    @FXML
+    private ChoiceBox<?> txtProvince;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    private TextField txtTitle;
+
+    @FXML
+    void btnAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEmployeeManage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemManage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierManage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdate(ActionEvent event) {
+
+    }
+
 }
---
+++ src/main/java/Controller/Customer/CustomerService.java
@@ -0,0 +1,12 @@
+package Controller.Customer;
+
+import javafx.collections.ObservableList;
+import model.Customer;
+
+public interface CustomerService {
+    void addCustomer(Customer customer);
+    void deleteCustomer(String custID);
+    void updateCustomer(Customer customer);
+
+    ObservableList<Customer> getAllCustomerDetails();
+}
